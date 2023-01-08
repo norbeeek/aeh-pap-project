787333846391b6552f9b79f7c160a1ba37e73484
hihtekv2 <58048410+hihtekv2@users.noreply.github.com>
2023-01-08 16:04:28 +0100

    Delete its_working!/projekt directory

--- its_working!/projekt/aeh-pap-project-master/main.py
+++
@@ -1,9 +0,0 @@
-from website import create_app
-import ssl
-
-app = create_app()
-
-if __name__ == '__main__':
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain('D:\projekt\MySQL\cert.pem', 'D:\projekt\MySQL\key.pem')
-    app.run(host='0.0.0.0', port='443', ssl_context=context)
